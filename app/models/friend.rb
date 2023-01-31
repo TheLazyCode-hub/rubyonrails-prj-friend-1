0ec5e60d9dba42bfe963d50727ac79f0431b26be
TheLazyCode-hub <gauravshukla052@gmail.com>
2023-01-31 21:45:36 +0530

    server and client side form validation done

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,11 @@
 class Friend < ApplicationRecord
     belongs_to :user
+    validates :first_name, :email, :phone, :address, presence: true
+    validates :email, uniqueness: true
+    validates :first_name, length: {minimum:2, maximum:50}
+    validates :first_name, format: {with: /\A[a-zA-Z]+\z/, message: "- Only letters allowed in name"}
+    validates :phone, numericality: true, length: {minimum:10, maximum:12}
+    validates :email, format: Devise.email_regexp
+    validates :address, length: {minimum:4, maximum:50}
+    
 end
